89d6b2aef1ae06736052edf3eef988147b6adf8e
wangshuqin <shuqin.wang@newtouch.com>
2023-12-13 15:54:53 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+let router = createRouter({
+  history: createWebHistory(),
+  routes: [
+  
+  ],
+})
+
+export default router;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html", "./App.vue", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
