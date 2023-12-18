cc1f69d9257737c7f222ae12fef42eb25f551f15
wangshuqin <shuqin.wang@newtouch.com>
2023-12-18 16:35:03 +0800

    font-size

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,20 +3,20 @@
     <div class="flex flex-wrap justify-between">
       <div class="mb-[30px]">
         <div class="flex items-center">
-          <img src="@/assets/images/logo.png" class="w-[60px] h-[60px]" />
-          <div class="text-[24px] text-[#6CD3FF] ml-[10px] w-[100px] leading-[26px]">AN Universe</div>
+          <img src="@/assets/images/logo.png" class="md:w-[60px] md:h-[60px] w-[40px] h-[40px]" />
+          <div class="md:text-[24px] text-[20px] text-[#6CD3FF] ml-[10px] w-[100px] leading-[26px]">AN Universe</div>
         </div>
         <div class="text-[#A690FC] text-[16px] mt-[20px] w-[260px] leading-[35px]">Ignite Your Intelligence in the
           AI-Enhanced Universe!</div>
       </div>
       <div class="text-[#E2E1E4] text-[16px] md:leading-[35px] leading-[28px] flex flex-wrap">
-        <div class="w-[200px] mb-[50px]">
+        <div class="w-[200px] mb-[30px]">
           <div class="text-[18px] md:mb-[20px] mb-[10px]">Community</div>
           <div>Discord</div>
           <div>Telegram</div>
           <div>WeChat</div>
         </div>
-        <div class="w-[200px] mb-[50px]">
+        <div class="w-[200px] mb-[30px]">
           <div class="text-[18px] md:mb-[20px] mb-[10px]">About us</div>
           <div>Our Story</div>
           <div>Career</div>
@@ -30,7 +30,7 @@
       </div>
     </div>
     <div
-      class="border border-solid border-x-0 border-b-0 border-[#2B2931] flex justify-between pt-[30px] md:px-[40px] mt-[80px]">
+      class="border border-solid border-x-0 border-b-0 border-[#2B2931] flex justify-between pt-[30px] md:px-[40px] md:mt-[80px] mt-[40px]">
       <div class="text-[#A7A4AF] text-[13px]">© Draft 2023, All Rights Reserved.</div>
       <div class="flex">
         <img src="@/assets/images/footer1.png" class="w-[16px] h-[16px]" />
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -3,23 +3,23 @@
     <div class="main-content">
       <Header></Header>
       <div class="">
-        <div class="md:h-[625px] pt-[30px] md:pt-[0px] flex items-center index-bg1 ">
+        <div class="md:h-[625px] pt-[38px] md:pt-[0px] flex items-center index-bg1 ">
           <div class="w-[580px]">
             <div class="text-[24px] md:text-[48px] leading-[42px] md:leading-[84px] font-bold">
               <div class="text-[#FFFFFF]">Unlock Wisdom, Join</div>
               <div class="text-color-css">An-Universe</div>
             </div>
-            <div class="md:text-[22px] text-[11px] leading-[35px] mt-[20px] mb-[40px] text-[#EEE9E9]">Return the value of
+            <div class="md:text-[22px] text-[16px] leading-[35px] mt-[20px] mb-[40px] text-[#EEE9E9]">Return the value of
               knowledge to
               the
               one who create it！AIGC is helping us better realize this vision. Let’s build a universe driving by AI-Avatar
               and
               knowledge together!</div>
-            <button class="btn-css text-xs md:text-base w-[75px] h-[27px] md:h-[55px] md:w-[150px]">Start Now</button>
+            <button class="btn-css text-xs md:text-base w-[120px] h-[35px] md:h-[55px] md:w-[150px]">Start Now</button>
           </div>
         </div>
         <div>
-          <div class="text-[24px] md:text-[48px] font-extrabold text-[#EBE6FA] text-center mt-[60px] md:mt-[120px]">Our
+          <div class="text-[24px] md:text-[48px] font-extrabold text-[#EBE6FA] text-center mt-[80px] md:mt-[120px]">Our
             Top Planets</div>
           <div class="text-[18px] text-[#E2E1E4] text-center">We have been partner with many influencers</div>
           <div class="flex mt-[50px] justify-center items-center">
@@ -42,7 +42,7 @@
         </div>
       </div>
       <div>
-        <div class="mt-[60px] md:mt-[120px] flex justify-between">
+        <div class="mt-[80px] md:mt-[120px] flex justify-between">
           <div class="hidden md:block">
             <img src="@/assets/images/Component5.png" class="w-[263px] ml-[30px]" />
             <div class="bg-[#291C52] rounded-full flex ml-[270px] p-[8px]">
@@ -53,16 +53,16 @@
             <div class="flex justify-center">
               <img src="@/assets/images/Component4.png" class="w-[124px]" />
             </div>
-            <div class="text-[24px] md:text-[48px] font-bold text-[#FFFFFF] mt-[20px] px-[60px] md:px-0">
+            <div class="text-[24px] md:text-[48px] font-bold text-[#FFFFFF] mt-[20px]">
               To access to Star Creator <label class="text-color-css">AI-Avatar</label>,<br>Just buy the Key.
             </div>
           </div>
         </div>
-        <div class="mt-[60px] md:mt-[120px] md:flex md:justify-between">
+        <div class="mt-[80px] md:mt-[120px] md:flex md:justify-between">
           <img src="@/assets/images/Component7.png"
             class="mx-auto md:mx-0 w-[50%] md:w-[100%] md:w-[608px] md:ml-[30px]" />
           <div class="md:flex md:items-center md:mr-[120px]">
-            <div class="px-[60px] md:px-0 md:w-[680px]">
+            <div class="md:w-[680px]">
               <div class="text-[24px] md:text-[48px] font-bold text-[#FFFFFF] leading-[42px] md:leading-[84px]">Explore
                 Diverse Knowledge Planets now!</div>
               <div class="text-[16px] md:text-[22px] text-[#EEE9E9] mt-[20px] italic">
@@ -71,14 +71,14 @@
                 <div class="mt-[30px]">Connect directly with star creators whose content resonates with you. Interact, ask
                   questions, and be part of a community where your engagement matters.</div>
               </div>
-              <button class="btn-css text-xs md:text-base h-[27px] w-[75px] md:h-[55px] md:w-[150px] mt-[30px]">Try It
+              <button class="btn-css text-xs md:text-base h-[35px] w-[120px] md:h-[55px] md:w-[150px] mt-[30px]">Try It
                 Now</button>
             </div>
           </div>
         </div>
-        <div class="mt-[60px] md:mt-[120px] flex md:flex-nowrap flex-wrap-reverse md:justify-between">
+        <div class="mt-[80px] md:mt-[120px] flex md:flex-nowrap flex-wrap-reverse md:justify-between">
           <div class="md:flex md:items-center md:ml-[120px]">
-            <div class="px-[60px] md:px-0 md:w-[680px]">
+            <div class=" md:w-[680px]">
               <div class="text-[24px] md:text-[48px] font-bold text-[#FFFFFF] leading-[42px] md:leading-[84px]">Create to
                 earn! Sharing your Knowledge.</div>
               <div class="text-[16px] md:text-[22px] text-[#EEE9E9] mt-[20px] italic">
@@ -88,7 +88,7 @@
                 <div class="mt-[30px]">Transform your passion into a profession. AN Universe empowers you to monetize your
                   knowledge and creations, offering a platform for financial growth and recognition.</div>
               </div>
-              <button class="btn-css text-xs md:text-base h-[27px] w-[75px] md:h-[55px] md:w-[150px] mt-[30px]">Try It
+              <button class="btn-css text-xs md:text-base h-[35px] w-[120px] md:h-[55px] md:w-[150px] mt-[30px]">Try It
                 Now</button>
             </div>
           </div>
@@ -138,7 +138,7 @@ import Footer from '../../components/Footer.vue'
 }
 
 .btn-css {
-  background-color: #FBFAFF;
+  background-color: #ffffff;
   color: #8C72FF;
   border-radius: 100px;
 }
