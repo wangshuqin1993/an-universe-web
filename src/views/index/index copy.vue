5081b6110376813c015ee1c2dd20dac847a60c70
wangshuqin <shuqin.wang@newtouch.com>
2023-12-29 16:08:43 +0800

    bmake

---
+++ src/views/index/index copy.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="w-full h-full px-[32px]">
+    <div class="main-content">
+      <Header></Header>
+      <div class="">
+        <div class="md:mt-[60px] pt-[38px] md:pt-[0px] flex md:justify-between md:items-center">
+          <div class="md:w-[580px]">
+            <div class="text-[24px] md:text-[48px] leading-[42px] md:leading-[84px] font-bold">
+              <div class="text-[#FFFFFF]">Unlock Wisdom, Join</div>
+              <div class="text-color-css">An-Universe</div>
+            </div>
+            <img src="@/assets/images/Component 1.png" class="md:hidden block w-[50%] mx-auto" />
+            <div class="md:text-[22px] text-[16px] leading-[35px] mt-[20px] mb-[40px] text-[#EEE9E9]">Return the value of
+              knowledge to the one who create it！AIGC is helping us better realize this vision.
+              Let’s build a universe driving by AI-Avatar and knowledge together!
+            </div>
+            <button class="btn-css text-xs md:text-base w-[120px] h-[35px] md:h-[55px] md:w-[150px]">Start Now</button>
+          </div>
+          <img src="@/assets/images/Component 1.png" class="hidden md:block md:w-[508px] md:mr-[30px]" />
+        </div>
+        <div>
+          <div
+            class="hidden md:block text-[24px] md:text-[48px] font-extrabold text-[#EBE6FA] text-center mt-[80px] md:mt-[120px]">
+            Our
+            Top Planets</div>
+          <div class="text-[18px] text-[#E2E1E4] text-center mt-[80px] md:mt-0">We have been partner with many influencers
+          </div>
+          <div class="flex mt-[50px] justify-center items-center md:px-[110px] px-[0px]">
+            <img src="@/assets/images/imgLogo1.png" class="img-w" />
+            <div class="img-w">
+              <img src="@/assets/images/imgLogo2.png" />
+              <img src="@/assets/images/imgLogo3.png" />
+            </div>
+            <div class="img-w">
+              <img src="@/assets/images/imgLogo4.png" />
+              <img src="@/assets/images/imgLogo5.png" />
+              <img src="@/assets/images/imgLogo6.png" />
+            </div>
+            <div class="img-w">
+              <img src="@/assets/images/imgLogo7.png" />
+              <img src="@/assets/images/imgLogo8.png" />
+            </div>
+            <img src="@/assets/images/imgLogo9.png" class="img-w" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="mt-[80px] md:mt-[120px] flex justify-between">
+          <div class="hidden md:block">
+            <img src="@/assets/images/Component5.png" class="w-[263px] ml-[30px]" />
+            <div class="bg-[#291C52] rounded-full flex ml-[270px] p-[8px]">
+              <img src="@/assets/images/untitled2.png" class="w-[33px] " />
+            </div>
+          </div>
+          <div class="md:mr-[30px]">
+            <div class="flex justify-center">
+              <img src="@/assets/images/Component4.png" class="w-[124px]" />
+            </div>
+            <div class="text-[24px] md:text-[48px] font-bold text-[#FFFFFF] mt-[20px]">
+              To access to Star Creator <label class="text-color-css">AI-Avatar</label>,<br>Just buy the Key.
+            </div>
+          </div>
+        </div>
+        <div class="mt-[80px] md:mt-[120px] md:flex md:justify-between">
+          <img src="@/assets/images/Component7.png"
+            class="mx-auto md:mx-0 w-[50%] md:w-[100%] md:w-[608px] md:ml-[30px]" />
+          <div class="md:flex md:items-center md:mr-[30px]">
+            <div class="md:w-[680px]">
+              <div class="text-[24px] md:text-[48px] font-bold text-[#FFFFFF] leading-[42px] md:leading-[84px]">Explore
+                Diverse Knowledge Planets now!</div>
+              <div class="text-[16px] md:text-[22px] text-[#EEE9E9] mt-[20px] italic">
+                <div>AN Universe opens the gateway to diverse knowledge universes. Explore stars across various fields,
+                  gaining insights and experiences that align with your interests.</div>
+                <div class="mt-[30px]">Connect directly with star creators whose content resonates with you. Interact, ask
+                  questions, and be part of a community where your engagement matters.</div>
+              </div>
+              <button class="btn-css text-xs md:text-base h-[35px] w-[120px] md:h-[55px] md:w-[150px] mt-[30px]">Try It
+                Now</button>
+            </div>
+          </div>
+        </div>
+        <div class="mt-[80px] md:mt-[120px] flex md:flex-nowrap flex-wrap-reverse md:justify-between">
+          <div class="md:flex md:items-center">
+            <div class=" md:w-[680px]">
+              <div class="text-[24px] md:text-[48px] font-bold text-[#FFFFFF] leading-[42px] md:leading-[84px]">Create to
+                earn! Sharing your Knowledge.</div>
+              <div class="text-[16px] md:text-[22px] text-[#EEE9E9] mt-[20px] italic">
+                <div> AN Universe is your canvas to share your knowledge and creativity. Benefit from the exclusive AI
+                  Avatar,
+                  enhancing your connection with fans.</div>
+                <div class="mt-[30px]">Transform your passion into a profession. AN Universe empowers you to monetize your
+                  knowledge and creations, offering a platform for financial growth and recognition.</div>
+              </div>
+              <button class="btn-css text-xs md:text-base h-[35px] w-[120px] md:h-[55px] md:w-[150px] mt-[30px]">Try It
+                Now</button>
+            </div>
+          </div>
+          <img src="@/assets/images/Component 8.png"
+            class="mx-auto md:mx-0 w-[50%] md:w-[100%] md:w-[608px] md:mr-[30px]" />
+        </div>
+        <div class="fixed right-[30px] bottom-[30px] cursor-pointer hidden md:block">
+          <a href="#top">
+            <div class="bg-[#EBE6FA] rounded-full h-[56px] w-[56px] flex justify-center items-center ml-[15px] mb-[10px]">
+              <img src="@/assets/images/Vector1.png" class="h-[16px]" />
+            </div>
+            <div class="text-[#EEE9E9]">Bact to Top</div>
+          </a>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+
+  </div>
+</template>
+<script setup lang="ts">
+import Header from '../../components/Header.vue'
+import Footer from '../../components/Footer.vue'
+</script>
+<style scoped>
+.main-content {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+.text-color-css {
+  width: max-content;
+  background: linear-gradient(245.29deg, #6DC1DC 13.32%, #A690FC 28.55%, #FC96BB 55.55%, #FFC397 84.12%), linear-gradient(0deg, #FFFFFF, #FFFFFF);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.img-w {
+  width: 20%;
+  height: fit-content;
+}
+
+.btn-css {
+  background-color: #ffffff;
+  color: #8C72FF;
+  border-radius: 100px;
+}
+</style>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,142 +1,58 @@
 <template>
-  <div class="w-full h-full px-[32px]">
-    <div class="main-content">
-      <Header></Header>
-      <div class="">
-        <div class="md:mt-[60px] pt-[38px] md:pt-[0px] flex md:justify-between md:items-center">
-          <div class="md:w-[580px]">
-            <div class="text-[24px] md:text-[48px] leading-[42px] md:leading-[84px] font-bold">
-              <div class="text-[#FFFFFF]">Unlock Wisdom, Join</div>
-              <div class="text-color-css">An-Universe</div>
-            </div>
-            <img src="@/assets/images/Component 1.png" class="md:hidden block w-[50%] mx-auto" />
-            <div class="md:text-[22px] text-[16px] leading-[35px] mt-[20px] mb-[40px] text-[#EEE9E9]">Return the value of
-              knowledge to the one who create it！AIGC is helping us better realize this vision.
-              Let’s build a universe driving by AI-Avatar and knowledge together!
-            </div>
-            <button class="btn-css text-xs md:text-base w-[120px] h-[35px] md:h-[55px] md:w-[150px]">Start Now</button>
-          </div>
-          <img src="@/assets/images/Component 1.png" class="hidden md:block md:w-[508px] md:mr-[30px]" />
-        </div>
+  <div class="w-full h-full px-[32px] main-content ">
+    <div class="max-w-[1440px] mx-auto">
+      <div class="flex justify-between  header h-[82px] py-[20px] ">
         <div>
-          <div
-            class="hidden md:block text-[24px] md:text-[48px] font-extrabold text-[#EBE6FA] text-center mt-[80px] md:mt-[120px]">
-            Our
-            Top Planets</div>
-          <div class="text-[18px] text-[#E2E1E4] text-center mt-[80px] md:mt-0">We have been partner with many influencers
-          </div>
-          <div class="flex mt-[50px] justify-center items-center md:px-[110px] px-[0px]">
-            <img src="@/assets/images/imgLogo1.png" class="img-w" />
-            <div class="img-w">
-              <img src="@/assets/images/imgLogo2.png" />
-              <img src="@/assets/images/imgLogo3.png" />
-            </div>
-            <div class="img-w">
-              <img src="@/assets/images/imgLogo4.png" />
-              <img src="@/assets/images/imgLogo5.png" />
-              <img src="@/assets/images/imgLogo6.png" />
-            </div>
-            <div class="img-w">
-              <img src="@/assets/images/imgLogo7.png" />
-              <img src="@/assets/images/imgLogo8.png" />
-            </div>
-            <img src="@/assets/images/imgLogo9.png" class="img-w" />
+          <img src="@/assets/images/mbake-logo.png" class="w-[140px]" />
+        </div>
+        <div class="flex items-stretch text-[#ffffff] text-[16px]">
+          <div class="mr-[50px] self-center">Whitepaper</div>
+          <div class="mr-[50px] self-center">Roadmap</div>
+          <div class="mr-[50px] self-center">IDO</div>
+          <div class="min-btn">
+            Mint (Coming Soon)
           </div>
         </div>
       </div>
-      <div>
-        <div class="mt-[80px] md:mt-[120px] flex justify-between">
-          <div class="hidden md:block">
-            <img src="@/assets/images/Component5.png" class="w-[263px] ml-[30px]" />
-            <div class="bg-[#291C52] rounded-full flex ml-[270px] p-[8px]">
-              <img src="@/assets/images/untitled2.png" class="w-[33px] " />
-            </div>
-          </div>
-          <div class="md:mr-[30px]">
-            <div class="flex justify-center">
-              <img src="@/assets/images/Component4.png" class="w-[124px]" />
-            </div>
-            <div class="text-[24px] md:text-[48px] font-bold text-[#FFFFFF] mt-[20px]">
-              To access to Star Creator <label class="text-color-css">AI-Avatar</label>,<br>Just buy the Key.
-            </div>
-          </div>
+      <div class="text-center content mt-[80px]">
+        <div class="text-[72px] content-title">
+          <span class="font-bold title-text">Bmaker</span>
         </div>
-        <div class="mt-[80px] md:mt-[120px] md:flex md:justify-between">
-          <img src="@/assets/images/Component7.png"
-            class="mx-auto md:mx-0 w-[50%] md:w-[100%] md:w-[608px] md:ml-[30px]" />
-          <div class="md:flex md:items-center md:mr-[30px]">
-            <div class="md:w-[680px]">
-              <div class="text-[24px] md:text-[48px] font-bold text-[#FFFFFF] leading-[42px] md:leading-[84px]">Explore
-                Diverse Knowledge Planets now!</div>
-              <div class="text-[16px] md:text-[22px] text-[#EEE9E9] mt-[20px] italic">
-                <div>AN Universe opens the gateway to diverse knowledge universes. Explore stars across various fields,
-                  gaining insights and experiences that align with your interests.</div>
-                <div class="mt-[30px]">Connect directly with star creators whose content resonates with you. Interact, ask
-                  questions, and be part of a community where your engagement matters.</div>
-              </div>
-              <button class="btn-css text-xs md:text-base h-[35px] w-[120px] md:h-[55px] md:w-[150px] mt-[30px]">Try It
-                Now</button>
-            </div>
-          </div>
-        </div>
-        <div class="mt-[80px] md:mt-[120px] flex md:flex-nowrap flex-wrap-reverse md:justify-between">
-          <div class="md:flex md:items-center">
-            <div class=" md:w-[680px]">
-              <div class="text-[24px] md:text-[48px] font-bold text-[#FFFFFF] leading-[42px] md:leading-[84px]">Create to
-                earn! Sharing your Knowledge.</div>
-              <div class="text-[16px] md:text-[22px] text-[#EEE9E9] mt-[20px] italic">
-                <div> AN Universe is your canvas to share your knowledge and creativity. Benefit from the exclusive AI
-                  Avatar,
-                  enhancing your connection with fans.</div>
-                <div class="mt-[30px]">Transform your passion into a profession. AN Universe empowers you to monetize your
-                  knowledge and creations, offering a platform for financial growth and recognition.</div>
-              </div>
-              <button class="btn-css text-xs md:text-base h-[35px] w-[120px] md:h-[55px] md:w-[150px] mt-[30px]">Try It
-                Now</button>
-            </div>
-          </div>
-          <img src="@/assets/images/Component 8.png"
-            class="mx-auto md:mx-0 w-[50%] md:w-[100%] md:w-[608px] md:mr-[30px]" />
-        </div>
-        <div class="fixed right-[30px] bottom-[30px] cursor-pointer hidden md:block">
-          <a href="#top">
-            <div class="bg-[#EBE6FA] rounded-full h-[56px] w-[56px] flex justify-center items-center ml-[15px] mb-[10px]">
-              <img src="@/assets/images/Vector1.png" class="h-[16px]" />
-            </div>
-            <div class="text-[#EEE9E9]">Bact to Top</div>
-          </a>
+        <div class="text-[18px] ">
+          <span class="title-text">The Web3 future Bitcoin Stablecoin Paradigm</span>
         </div>
       </div>
-      <Footer></Footer>
     </div>
 
   </div>
 </template>
 <script setup lang="ts">
-import Header from '../../components/Header.vue'
-import Footer from '../../components/Footer.vue'
 </script>
 <style scoped>
 .main-content {
+  height: 100Vh;
+  margin: 0;
+  background: url('../../assets/images/bmake.jpg') no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center center;
+  /* 
   max-width: 1440px;
-  margin: 0 auto;
+  margin: 0 auto; */
 }
 
-.text-color-css {
-  width: max-content;
-  background: linear-gradient(245.29deg, #6DC1DC 13.32%, #A690FC 28.55%, #FC96BB 55.55%, #FFC397 84.12%), linear-gradient(0deg, #FFFFFF, #FFFFFF);
+.min-btn {
+  line-height: 40px;
+  font-size: 14px;
+  padding: 0 12px;
+  border-radius: 5px;
+  background: linear-gradient(90deg, #6E56FF 0%, #F41FFF 100%);
+}
+
+.title-text {
+  background-image: linear-gradient(to right, #60638B 0%, #F9F9F9 25%, #FFFFFF 50%, #60638B 100%);
   -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-
-.img-w {
-  width: 20%;
-  height: fit-content;
-}
-
-.btn-css {
-  background-color: #ffffff;
-  color: #8C72FF;
-  border-radius: 100px;
+  background-clip: text;
+  color: transparent;
 }
 </style>
