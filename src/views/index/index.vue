493d670ea8d53582ac17cab20facfdf0407abfb1
shuqin wang <1440749324@qq.com>
2024-08-02 17:51:58 +0800

    add img

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -133,7 +133,7 @@
   <div class="bg-black px-[32px]">
     <div class="w-full text-[#ffffff] text-center md:pt-[120px] pt-[60px] max-w-[1428px] mx-auto">
       <div class="md:text-[48px] text-[24px] font-bold md:mb-[80px] mb-[48px]">
-        <div>Permissionless & Native Infra 4 BTC Liquidity & Payment</div>
+        <div>Inter-liquidity & Decentralized Statement Channel Protocol for Payfi</div>
       </div>
       <div class="md:grid items-center justify-around grid-cols-3 gap-12 md:flex md:px-[64px]">
         <div class="mb-[48px] md:mb-[0px]">
@@ -187,19 +187,21 @@
     </div>
   </div>
 
-  <div class="pt-[60px] text-[#ffffff] box-4 px-[32px] md:h-full h-[400px] md:pb-[200px]">
+  <div class="pt-[60px] text-[#ffffff] box-4 px-[32px] md:h-full h-[460px] md:pb-[200px]">
     <div class="md:text-[48px] text-[24px] font-bold md:mb-[80px] mb-[48px] text-center">Partnership</div>
     <div
-      class="mx-auto grid md:grid-cols-3 grid-cols-2 gap-4 box-4-in w-[100%] md:w-[1160px] md:h-[540px] items-center md:justify-items-center p-[24px] md:p-[100px] ">
-      <img src="@/assets/images/ANT.png" class="md:h-[64px] h-[36px]" />
-
-      <img src="@/assets/images/Pic.png" class="md:h-[64px] h-[36px]" />
-      <img src="@/assets/images/KODO.png" class="md:h-[64px] h-[36px]" />
-      <img src="@/assets/images/Syn.png" class="md:h-[48px] h-[30px]" />
-      <img src="@/assets/images/Par.png" class="md:h-[48px]  h-[26px]" />
-      <img src="@/assets/images/OKX.png" class="md:h-[48px] h-[26px]" />
+      class="mx-auto grid md:grid-cols-3 grid-cols-2 gap-4 box-4-in w-[100%] md:w-[1160px] md:h-[540px] items-center md:justify-items-left p-[24px] md:p-[100px] ">
+      <img src="@/assets/images/baby.png" class="md:h-[44px] h-[26px]" />
       <img src="@/assets/images/Photoroom.png" class="md:h-[48px] h-[26px]" />
       <img src="@/assets/images/Photoroom2.png" class="md:h-[48px] h-[26px]" />
+      <img src="@/assets/images/gg.png" class="md:h-[48px] h-[36px]" />
+      <img src="@/assets/images/KODO.png" class="md:h-[64px] h-[36px]" />
+      <img src="@/assets/images/Par.png" class="md:h-[48px]  h-[26px]" />
+      <img src="@/assets/images/ANT.png" class="md:h-[64px] h-[36px]" />
+      <img src="@/assets/images/Syn.png" class="md:h-[48px] h-[30px]" />
+      <img src="@/assets/images/OKX.png" class="md:h-[48px] h-[26px]" />
+
+      <img src="@/assets/images/Pic.png" class="md:h-[64px] h-[36px]" />
     </div>
   </div>
 
